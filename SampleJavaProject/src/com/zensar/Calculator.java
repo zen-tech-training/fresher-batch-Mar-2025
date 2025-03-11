27e87e2bfd3640c1ec80110dfcf5187679600ef0
NS115680 <naragantisunilkumar@gmail.com>
2025-03-11 12:03:01 +0530

    modified sub funtion

--- SampleJavaProject/src/com/zensar/Calculator.java
+++ SampleJavaProject/src/com/zensar/Calculator.java
@@ -5,14 +5,14 @@ public class Calculator {
 		return a + b;
 	}
 
-	public static int sub(int a, int b) {
-		return a - b;
+	public static void sub(int a, int b) {
+		System.out.print( a- b);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(add(2, 3));
-		System.out.println(sub(10, 5));
+		sub(10, 5);
 
 	}
 
