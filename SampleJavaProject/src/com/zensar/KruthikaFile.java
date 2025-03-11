30be4dd8adc6a29d04330dc9775b1b8f8d272958
AK115690 <AK115690@FREW69SLPC0138.ind.zensar.com>
2025-03-11 14:03:12 +0530

    Merge branch 'main' of https://github.com/zen-tech-training/fresher-batch-Mar-2025 into main

--- SampleJavaProject/src/com/zensar/KruthikaFile.java
+++ SampleJavaProject/src/com/zensar/KruthikaFile.java
@@ -1,9 +1,8 @@
 package com.zensar;
 
 public class KruthikaFile {
-
 	public static void main(String[] args) {
-		System.out.println("Have a Nice Day");
+		System.out.println("Have a nice day");
 	}
 
 }
