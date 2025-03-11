8c3d343d9b9767f5c46ab7a3fda9f6c12f792632
BM115693 <bukkapatnammadhusree2002@gmail.com>
2025-03-11 14:23:08 +0530

    Merge branch 'main' of https://github.com/zen-tech-training/fresher-batch-Mar-2025 into main

--- SampleJavaProject/src/com/zensar/Madhusree.java
+++ SampleJavaProject/src/com/zensar/Madhusree.java
@@ -1,9 +1,8 @@
 package com.zensar;
 
 public class Madhusree {
-
 	public static void main(String[] args) {
-		System.out.println("My name is Madhusree");
+		System.out.println("Madhusree");
 	}
 
 }
